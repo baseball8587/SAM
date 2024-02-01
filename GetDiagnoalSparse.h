5b466b2a482cd45430c8d86c08de791d27187397
Colin Jacobs <cbj225@lehigh.edu>
2024-01-31 19:40:05 -0500

    Working sequential implementation

---
+++ GetDiagnoalSparse.h
@@ -0,0 +1,16 @@
+#pragma once
+Eigen::SparseVector<double> getDiagonalSparse(const Eigen::SparseMatrix<double>& mat) {
+    Eigen::SparseVector<double> diagonal(mat.rows());
+
+    // Define the limit based on matrix dimensions
+    int limit = (mat.rows() <= mat.cols()) ? mat.rows() : mat.cols();
+
+    for (int k = 0; k < limit; ++k) {
+        double value = mat.coeff(k, k);
+        if (value != 0) {  // Only insert non-zero values
+            diagonal.insert(k) = value;
+        }
+    }
+
+    return diagonal;
+}
---
+++ readMatrix.h
@@ -0,0 +1,28 @@
+#pragma once
+// Function to read and return a SparseMatrix from a binary data pointer
+Eigen::SparseMatrix<double> readMatrix(char*& dataPtr, double& elapsed_time) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int len = *reinterpret_cast<int*>(dataPtr);
+    dataPtr += sizeof(int);  // Move the data pointer by the size of the integer
+
+    int* i_data = reinterpret_cast<int*>(dataPtr);    // Row indices
+    dataPtr += len * sizeof(int);
+    int* j_data = reinterpret_cast<int*>(dataPtr);    // Column indices
+    dataPtr += len * sizeof(int);
+    double* s_data = reinterpret_cast<double*>(dataPtr);  // Values
+    dataPtr += len * sizeof(double);
+
+    int max_row = *std::max_element(i_data, i_data + len) + 1; // +1 to convert to size
+    int max_col = *std::max_element(j_data, j_data + len) + 1;
+
+    Eigen::SparseMatrix<double> mat(max_row, max_col);
+    mat.reserve(len); // Reserve space for upcoming inserts
+    for (int index = 0; index < len; ++index) {
+        mat.insert(i_data[index], j_data[index]) = s_data[index];
+    }
+
+  
+
+    return mat;
+}
